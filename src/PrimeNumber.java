fec9cb45167b95fcdd7f9cfdc19f67001ab042ad
linhbh25796 <linhbh25796@gmail.com>
2019-09-13 10:04:49 +0700

    Prime number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+
+    /**
+     * funtion check prime number
+     *
+     * @param n
+     * @return  true : is prime
+     *          false: is not prime
+     */
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 0; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+
+}
